deda0a7372cb2861f0403274cb535d99d8b962f2
Elisei <brunoelisei@o2ti.com>
2025-02-20 10:03:47 -0300

    O2TI 😍 Magento - Dev de PLP - Desassocie order de uma PLP

---
+++ Ui/Component/Listing/Column/PlpOrderActions.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * O2TI Sigep Web Carrier.
+ *
+ * Copyright © 2025 O2TI. All rights reserved.
+ *
+ * @author    Bruno Elisei <brunoelisei@o2ti.com>
+ * @license   See LICENSE for license details.
+ */
+
+namespace O2TI\SigepWebCarrier\Ui\Component\Listing\Column;
+
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\UrlInterface;
+
+class PlpOrderActions extends Column
+{
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * Constructor
+     *
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                if (isset($item['entity_id'])) {
+                    $item[$this->getData('name')] = [
+                        'remove' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                'sigepweb/plp/removeOrder',
+                                [
+                                    'plp_id' => $item['plp_id'],
+                                    'order_id' => $item['order_id']
+                                ]
+                            ),
+                            'label' => __('Remove'),
+                            'confirm' => [
+                                'title' => __('Remove Order from PLP'),
+                                'message' => __('Are you sure you want to remove this order from the PLP?')
+                            ]
+                        ]
+                    ];
+                }
+            }
+        }
+        return $dataSource;
+    }
+}
